6f51acde3d431c396e76f515d94d0a444cecc1dc
jvill171 <juanvillarreal64@yahoo.com>
2023-08-05 19:29:30 -0500

    Refactored code to function with useAxios custom hook. Added helper functions to clean up code and make desired formats for data possible

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -1,28 +1,24 @@
 import React, { useState } from "react";
-import {v1 as uuid} from "uuid";
-import axios from "axios";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
+import { useAxios } from "./hooks";
+import { formatCard } from "./helpers";
+
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-  const [cards, setCards] = useState([]);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
-    );
-    setCards(cards => [...cards, { ...response.data, id: uuid() }]);
-  };
+  const BASE_URL = "https://deckofcardsapi.com/api/deck/new/draw/";
+  const [cards, setCards] = useAxios(BASE_URL);
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
-        <button onClick={addCard}>Add a playing card!</button>
+        <button onClick={()=> setCards(formatCard)}>Add a playing card!</button>
       </div>
       <div className="PlayingCardList-card-area">
         {cards.map(cardData => (
-          <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
+          <PlayingCard key={cardData.id} front={cardData.image} />
         ))}
       </div>
     </div>
--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,33 @@
+import {v1 as uuid} from "uuid";
+
 /* Select a random element from values array. */
 function choice(values) {
   const randIdx = Math.floor(Math.random() * values.length);
   return values[randIdx];
 }
 
-export { choice };
+// Desired format for Playing Cards
+function formatCard(data){
+  return {
+    id: uuid(),
+    image: data.cards[0].image
+  }
+}
+
+// Desired format for Pokemon Cards
+function formatPokemon(data){
+  let rObj = {
+    id: uuid(),
+    front: data.sprites.front_default,
+    back: data.sprites.back_default,
+    name: data.name,
+    stats: data.stats.map(stat => ({
+      value: stat.base_stat,
+      name: stat.stat.name
+    }))
+  }
+
+  return rObj
+}
+
+export { choice, formatCard, formatPokemon };
